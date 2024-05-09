3fb9313c4b00a026c09890a090dc260e1bf661eb
Janno Tjarks <janno.tjarks@mailbox.org>
2024-05-09 14:29:42 +0200

    docs: Better commenting of the keymaps

--- lua/janno/keymaps.lua
+++ lua/janno/keymaps.lua
@@ -1,12 +1,12 @@
 local map = require("janno.utils").map
 
--- Disables the arrow keys to learn hjkl
+-- Disables the arrow keys to force the usage of hjkl
 map("n", "<up>", "<nop>")
 map("n", "<down>", "<nop>")
 map("n", "<left>", "<nop>")
 map("n", "<right>", "<nop>")
 
--- Move a single line up or down
+-- Move line(s) up or down
 -- Explanation for the maps below
 -- https://stackoverflow.com/questions/69264441/is-there-a-way-to-copy-a-selected-area-up-or-down-in-vim
 map("n", "<A-k>", "<cmd>m-2<CR>")
@@ -14,7 +14,7 @@ map("n", "<A-j>", "<cmd>m+1<CR>")
 map("v", "<A-k>", ":m '<-2<CR>gv")
 map("v", "<A-j>", ":m '>+1<CR>gv")
 
--- Change buffer
+-- Switch buffer
 map("n", "<A-.>", "<cmd>bnext<CR>")
 map("n", "<A-,>", "<cmd>bprevious<CR>")
 map("n", "<A-c>", "<cmd>bdelete<CR>")
