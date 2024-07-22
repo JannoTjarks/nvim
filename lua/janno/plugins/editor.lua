68def6c6041882e8ac937164b922c3d7c41be86a
Janno Tjarks <janno.tjarks@mailbox.org>
2024-07-22 23:58:30 +0200

    feat(editor): add outline.nvim

--- lua/janno/plugins/editor.lua
+++ lua/janno/plugins/editor.lua
@@ -259,4 +259,16 @@ return {
             end, { desc = "Open parent directory" })
         end,
     },
+    {
+        "hedyhli/outline.nvim",
+        config = function()
+            -- Example mapping to toggle outline
+            vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
+                { desc = "Toggle Outline" })
+
+            require("outline").setup {
+                -- Your setup opts here (leave empty to use defaults)
+            }
+        end,
+    },
 }
