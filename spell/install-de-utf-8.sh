6a782ad501c395d161af3b6de482ea5e97ee1167
Janno Tjarks <janno.tjarks@mailbox.org>
2024-08-22 21:42:12 +0200

    feat: adds script do download spelllang de-utf8 and first version of de.utf-8.add

---
+++ spell/install-de-utf-8.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -Lo ~/.config/nvim/spell/de.utf-8.spl http://ftp.vim.org/vim/runtime/spell/de.utf-8.spl
+curl -Lo ~/.config/nvim/spell/de.utf-8.sug http://ftp.vim.org/vim/runtime/spell/de.utf-8.sug
