4a2f9a41349d4c47382fed984a2fb62ade44dcb1
Janno Tjarks <janno.tjarks@mailbox.org>
2025-01-06 07:04:56 +0100

    feat: remove hedyhli/outline.nvim and add ThePrimeagen/harpoon and epwalsh/obsidian.nvim

--- lua/janno/plugins/editor.lua
+++ lua/janno/plugins/editor.lua
@@ -212,20 +212,48 @@ return {
         end,
     },
     {
-        "hedyhli/outline.nvim",
-        config = function()
-            -- Example mapping to toggle outline
-            vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
-
-            require("outline").setup({
-                -- Your setup opts here (leave empty to use defaults)
-            })
-        end,
-    },
-    {
         "MagicDuck/grug-far.nvim",
         config = function()
             require("grug-far").setup({})
         end,
+    },
+    {
+        "epwalsh/obsidian.nvim",
+        version = "*", -- recommended, use latest release instead of latest commit
+        lazy = true,
+        ft = "markdown",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        opts = {
+            workspaces = {
+                {
+                    name = "jannos notes",
+                    path = "~/Documents/notes/obsidian/Jannos Notes/",
+                },
+            },
+        },
+    },
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local harpoon = require("harpoon")
+
+            harpoon:setup()
+
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+            vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+
+            -- Toggle previous & next buffers stored within Harpoon list
+            vim.keymap.set("n", "<leader>.", function() harpoon:list():next() end)
+            vim.keymap.set("n", "<leader>,", function() harpoon:list():prev() end)
+        end
     },
 }
