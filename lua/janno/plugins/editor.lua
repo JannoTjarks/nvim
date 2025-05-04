292bf7e5b8a273321a8e3668750492791cce6498
Janno Tjarks <janno.tjarks@mailbox.org>
2025-05-04 12:07:23 +0200

    chore: set <leader>c to clear harpoon list

--- lua/janno/plugins/editor.lua
+++ lua/janno/plugins/editor.lua
@@ -244,6 +244,7 @@ return {
             harpoon:setup()
 
             vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+            vim.keymap.set("n", "<leader>c", function() harpoon:list():clear() end)
             vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
             vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
