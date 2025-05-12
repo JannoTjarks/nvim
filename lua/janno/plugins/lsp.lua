aeb5adf36ac5c42f10d4b94bc25427e8a99c0167
Janno Tjarks <janno.tjarks@mailbox.org>
2025-05-12 08:05:21 +0200

    chore: remove lsp signs because current approuch is deprecated and will removed in v0.12

--- lua/janno/plugins/lsp.lua
+++ lua/janno/plugins/lsp.lua
@@ -141,12 +141,6 @@ return {
                 require("cmp_nvim_lsp").default_capabilities()
             )
 
-            local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-            for type, icon in pairs(signs) do
-                local hl = "DiagnosticSign" .. type
-                vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-            end
-
             local lsp = require("lspconfig")
 
             lsp.bashls.setup({
